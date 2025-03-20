14af9939d15bd82ae3e64a6fb01fcfa7c3d31404
Marziap <marziapirozzi2002@gmail.com>
2025-03-20 17:14:08 +0100

    added sliders to position better the scene, completed computer logic, added hover effect on interactable objects, added possibility to hide book anchor

--- EscapeRoom/ContentView.swift
+++ EscapeRoom/ContentView.swift
@@ -4,9 +4,123 @@ import RealityKitContent
 
 struct ContentView: View {
     @Environment(AppModel.self) private var appModel
+    @State private var translationx: Float = 0
+    @State private var translationy: Float = 0
+    @State private var translationz: Float = 0
+    @State private var rotationx: Float = 0
+    @State private var rotationy: Float = 0
+    @State private var rotationz: Float = 0
+    @State private var angle: Float = 0
     
     var body: some View {
+        VStack{
+            ToggleImmersiveSpaceButton()
+            
+            if appModel.immersiveSpaceState == .open {
+                Form {
+                    Section("Translation"){
+                        HStack {
+                            Text("Translate on x")
+                                .font(.title)
+                            Slider(value: $translationx, in: -10...10)
+                                .onChange(of: translationx) {
+                                    appModel.translation.x = translationx
+                                }
+                            Text("\(translationx)")
+                                .font(.title)
+                        }
+                        
+                        HStack {
+                            Text("Translate on y")
+                                .font(.title)
+                            Slider(value: $translationy, in: -10...10)
+                                .onChange(of: translationy) {
+                                    appModel.translation.y = translationy
+                                }
+                            Text("\(translationy)")
+                                .font(.title)
+                        }
+                        
+                        HStack {
+                            Text("Translate on z")
+                                .font(.title)
+                            Slider(value: $translationz, in: -10...10)
+                                .onChange(of: translationz) {
+                                    appModel.translation.z = translationz
+                                }
+                            Text("\(translationz)")
+                                .font(.title)
+                        }
+                    }
+                    
+                    Section("Rotation") {
+                        
+                        HStack {
+                            Text("Rotate on x")
+                                .font(.title)
+                            Slider(value: $rotationx, in: -10...10)
+                                .onChange(of: rotationx) {
+                                    appModel.rotation.vector.x = rotationx
+                                }
+                            Text("\(rotationx)")
+                                .font(.title)
+                        }
+                        
+                        HStack {
+                            Text("Rotate on y")
+                                .font(.title)
+                            Slider(value: $rotationy, in: -10...10)
+                                .onChange(of: rotationy) {
+                                    appModel.rotation.vector.y = rotationy
+                                }
+                            Text("\(rotationy)")
+                                .font(.title)
+                        }
+                        
+                        HStack {
+                            Text("Rotate on z")
+                                .font(.title)
+                            Slider(value: $rotationz, in: -10...10)
+                                .onChange(of: rotationz) {
+                                    appModel.rotation.vector.z = rotationz
+                                }
+                            Text("\(rotationz)")
+                                .font(.title)
+                        }
+                    }
+                    
+                    
+                }
+                
+                Button {
+                    appModel.translation = [0,0,0]
+                    appModel.rotation.vector = [0,0,0,0]
+                } label: {
+                    Text("Reset")
+                }
+                
+                Button {
+                    appModel.bookAnchor.toggle()
+                } label: {
+                    appModel.bookAnchor ? Text("Disable book anchor") : Text("Enable book anchor")
+                }
+                
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+        }.padding(.vertical, 20)
+            .frame(width: 800)
         
-        ToggleImmersiveSpaceButton()
     }
+}
+
+#Preview {
+    ContentView()
+        .environment(AppModel())
 }
--- EscapeRoom/ImmersiveView.swift
+++ EscapeRoom/ImmersiveView.swift
@@ -14,9 +14,15 @@ struct ImmersiveView: View {
     
     @Observable
     final class StateManager {
-        var myTexture: TextureResource? = nil
+        var textures: [TextureResource?] = []
         var sceneModel: Entity? = nil
         var schermoModel: Entity? = nil
+        var loadedTexture: TextureResource? = nil
+        var changeScreen: Bool = false
+        var startCaptchas: Bool = false
+        var counter: Int = 1
+        var bookModel: Entity? = nil
+        var handAnchor: AnchorEntity? = nil
     }
     
     @Environment(AppModel.self) private var appModel
@@ -31,17 +37,9 @@ struct ImmersiveView: View {
                let schermo = immersiveContentEntity.findEntity(named: "Schermo") as? ModelEntity {
                 self.stateManager.sceneModel = immersiveContentEntity
                 self.stateManager.schermoModel = schermo
+                
                 content.add(immersiveContentEntity)
-                
-                print("found schermo")
-                if let texture = try? TextureResource.load(named: "Captcha1") {
-                    print("found image")
-                    print("texture found: \(texture)")
-                    
-                    self.stateManager.myTexture = texture
-                    print("my texture: \(self.stateManager.myTexture)")
-                }
-                
+                loadTextures()
             }
             
             let session = SpatialTrackingSession()
@@ -51,64 +49,197 @@ struct ImmersiveView: View {
             
             
             //Setup an anchor at the user's right palm.
-            let handAnchor = AnchorEntity(.hand(.left, location: .palm), trackingMode: .continuous)
+            stateManager.handAnchor = AnchorEntity(.hand(.left, location: .palm), trackingMode: .continuous)
             
             //Add the Gauntlet scene that was set up in Reality Composer Pro.
             if let bookEntity = try? await Entity(named: "booknoteFinal", in: realityKitContentBundle) {
                 
+                stateManager.bookModel = bookEntity
                 
-                //Child the gauntlet scene to the handAnchor
-                handAnchor.addChild(bookEntity)
                 
-                // Add the handAnchor to the RealityView scene.
-                content.add(handAnchor)
+            }
+            
+            if let entity = stateManager.sceneModel?.findEntity(named: "Mug_Cylinder_013_Cylinder_007_001"){
+                print("child: \(entity.name)")
+                if(entity.components.has(InputTargetComponent.self)){
+                    entity.components.set(HoverEffectComponent(.spotlight(
+                        HoverEffectComponent.SpotlightHoverEffectStyle(
+                            color: .yellow, strength: 3.0
+                        )
+                    )))
+                }
+            }
+            
+            if let entity = stateManager.sceneModel?.findEntity(named: "Cylinder_002"){
+                print("child: \(entity.name)")
+                if(entity.components.has(InputTargetComponent.self)){
+                    entity.components.set(HoverEffectComponent(.spotlight(
+                        HoverEffectComponent.SpotlightHoverEffectStyle(
+                            color: .yellow, strength: 2.0
+                        )
+                    )))
+                }
+            }
+            
+            if let entity = stateManager.sceneModel?.findEntity(named: "Cylinder_003"){
+                print("child: \(entity.name)")
+                if(entity.components.has(InputTargetComponent.self)){
+                    entity.components.set(HoverEffectComponent(.spotlight(
+                        HoverEffectComponent.SpotlightHoverEffectStyle(
+                            color: .yellow, strength: 2.0
+                        )
+                    )))
+                }
+            }
+            
+            if let entity = stateManager.sceneModel?.findEntity(named: "Cylinder_005"){
+                print("child: \(entity.name)")
+                if(entity.components.has(InputTargetComponent.self)){
+                    entity.components.set(HoverEffectComponent(.spotlight(
+                        HoverEffectComponent.SpotlightHoverEffectStyle(
+                            color: .yellow, strength: 2.0
+                        )
+                    )))
+                }
             }
             
             
         }update: {content in
             
-            guard let texture = self.stateManager.myTexture,
-                  let schermoModel = self.stateManager.schermoModel else { return }
             
-            print("after guard")
-            if(appModel.switchScreen){
-                print("inside if")
+            if(appModel.bookAnchor){
+                //                    Child the gauntlet scene to the handAnchor
+                stateManager.handAnchor!.addChild(stateManager.bookModel!)
+                
+                //                     Add the handAnchor to the RealityView scene.
+                content.add(stateManager.handAnchor!)
+            }else{
+                content.remove(stateManager.handAnchor!)
+            }
+            
+            stateManager.sceneModel!.transform.translation = appModel.translation
+            stateManager.sceneModel!.transform.rotation = appModel.rotation
+            
+            if(stateManager.startCaptchas){
+                
+                guard let texture = stateManager.textures[0],
+                      let schermoModel = stateManager.schermoModel else { return }
+                
+                stateManager.loadedTexture = texture
+                
                 var material = SimpleMaterial()
                 material.color = .init(tint: .white, texture: .init(texture))
-                print("modified material color")
+                //print("modified material color")
                 
                 if var modelComponent = schermoModel.components[ModelComponent.self] {
-                    print("copied component")
+                    // print("copied component")
                     modelComponent.materials.removeAll()
-                    print("removed all material")
+                    // print("removed all material")
                     modelComponent.materials = [material]
-                    print("added material")
+                    // print("added material")
                     schermoModel.components[ModelComponent.self] = modelComponent
-                    print("assigned material")
-                    //content.add(schermoModel)
+                    //  print("assigned material")
                 }
+                
+                stateManager.startCaptchas = false
             }
+            
+            if(stateManager.changeScreen == true && stateManager.loadedTexture != nil && stateManager.counter < stateManager.textures.count){
+                
+                //get next texture
+                guard let texture = stateManager.textures[stateManager.counter],
+                      let schermoModel = stateManager.schermoModel else { return }
+                
+                stateManager.loadedTexture = texture
+                
+                var material = SimpleMaterial()
+                material.color = .init(tint: .white, texture: .init(texture))
+                // print("modified material color")
+                
+                if var modelComponent = schermoModel.components[ModelComponent.self] {
+                    //  print("copied component")
+                    modelComponent.materials.removeAll()
+                    //  print("removed all material")
+                    modelComponent.materials = [material]
+                    //  print("added material")
+                    schermoModel.components[ModelComponent.self] = modelComponent
+                    //  print("assigned material")
+                }
+                
+                stateManager.changeScreen = false
+                stateManager.counter += 1
+            }
+            
+            
             
             
         }.installGestures()
         
         //.upperLimbVisibility(.hidden)
         
-        .gesture(
-            SpatialTapGesture()
-            //                .targetedToAnyEntity()
-                .targetedToEntity(where: .has(InputTargetComponent.self))
-                .onEnded {
-                    $0.entity.applyTapForBehaviors()
-                    if($0.entity.name == "Cylinder_002"){
-                        print("wowowowowo")
-                        print(appModel.switchScreen)
+            .gesture(
+                SpatialTapGesture()
+                    .targetedToEntity(where: .has(InputTargetComponent.self))
+                    .onEnded {
+                        $0.entity.applyTapForBehaviors()
+                        if($0.entity.name == "Cylinder_002" && stateManager.loadedTexture == nil){
+                            // print("started captchas")
+//                            print(stateManager.startCaptchas)
+                            stateManager.startCaptchas = true
+//                            print(stateManager.startCaptchas)
+                        }
                         
-                        appModel.switchScreen.toggle()
-                        print(appModel.switchScreen)
-                        
-                    }
-                })
+                        if(stateManager.loadedTexture != nil){
+                            stateManager.changeScreen = true
+                        }
+                    })
+    }
+    
+    
+    
+    
+    
+    func loadTextures (){
+        if let texture = try? TextureResource.load(named: "Captcha1") {
+            //print("texture1 found")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha1Wrong") {
+            // print("texture1Wrong found")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha2") {
+            // print("texture2 found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha2Wrong") {
+            //  print("texture2Wrong found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha3") {
+            //  print("texture3 found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha3Wrong") {
+            //  print("texture3Wrong found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha4") {
+            // print("texture4 found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha4Wrong") {
+            //            print("texture4Wrong found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha5") {
+            //            print("texture5 found:")
+            stateManager.textures.append(texture)
+        }
+        if let texture = try? TextureResource.load(named: "Captcha5Wrong") {
+            //            print("texture5Wrong found:")
+            stateManager.textures.append(texture)
+        }
     }
     
 }
